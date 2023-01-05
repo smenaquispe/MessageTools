2060622299922b06cd16e3e363b85e0613385681
smenaq <sergiosantosmenaq@gmail.com>
2023-01-05 09:12:21 -0500

    send a message with a post

---
+++ src/API/Routes/message.routes.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const { client } = require('../../Client/client.js');
+
+const message_route = new Router()
+
+/**
+ * send a message to a contact
+ */
+message_route.post('/message/send', async (req, res) => {
+    try {
+        const { destinatary, message } = req.body;
+
+        const sending_respone = {
+            'message' : 'the message sent correctly',
+            'status' : 'sent to a current chat'
+        }
+
+        const chat = (await client.getChats()).find(ch => ch.id.user == destinatary);
+        if(chat === undefined)  sending_respone['status'] = 'sent to a new chat';
+
+        const chatId = chat.id.user + "@c.us";
+        await client.sendMessage(chatId,message);
+        res.send(sending_respone);
+
+    } catch (error) {
+        console.log(error)
+        res.send({'error' : error})
+    }
+})
+
+exports.message_route = message_route
